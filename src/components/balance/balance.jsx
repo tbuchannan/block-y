1234774a3180abca11b84594eb8d4a558ce3d6da
Terence Buchannan <terencebuchannan@gmail.com>
2018-02-19 01:25:47 -0500

    Balance: Remove Loader

--- src/components/balance/balance.jsx
+++ src/components/balance/balance.jsx
@@ -3,34 +3,29 @@ import '../../styles/balance.css';
 import Paper from 'material-ui/Paper';
 import { List, ListItem } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
-import CircularProgress from 'material-ui/CircularProgress';
 
 class Balance extends React.Component {
+  // TODO: refactor keys, to be more unique and not hardcoded
   render() {
-    const {info, isLoading} = this.props;
-    return isLoading ? (
-      <div>
-        <br/>
-        <CircularProgress />
-      </div>
-    ) :
-    info.address ? (
+    const {info} = this.props;
+    const balanceKeys = [1, 2, 3, 4, 5];
+    const balanceInfo = !info.address ? [] : [
+      <Divider />,
+      <ListItem key={balanceKeys[0]}>Address: {info.address}</ListItem>,
+      <ListItem key={balanceKeys[1]}>Total Received: {info.total_received / 100000000} BTC</ListItem>,
+      <ListItem key={balanceKeys[2]}>Total Sent: {info.total_sent / 100000000} BTC</ListItem>,
+      <ListItem key={balanceKeys[3]}>Remaining Balance: {info.final_balance / 100000000} BTC</ListItem>,
+    ];
+
+    return (
       <div className="balance">
         <Paper className="paper" zDepth={1} >
           <List>
-            <label>Balance</label>
-            <Divider />
-            <ListItem
-              nestedItems={[
-                <ListItem key={info.address}>Address: {info.address}</ListItem>,
-                <ListItem key={info.total_received}>Total Received: {info.total_received / 100000000} BTC</ListItem>,
-                <ListItem key={info.total_sent}>Total Sent: {info.total_sent / 100000000} BTC</ListItem>,
-                <ListItem key={info.final_balance}>Remaining Balance: {info.final_balance / 100000000} BTC</ListItem>,
-              ]}/>
+            <ListItem key={balanceKeys[4]} nestedItems={balanceInfo}>Balance</ListItem>
           </List>
         </Paper>
       </div>
-    ) : null;
+    );
   }
 }
 
