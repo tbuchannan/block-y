870f6d8145d851bd51e87b53fb9f64618ecfe174
Terence Buchannan <terencebuchannan@gmail.com>
2018-02-18 21:27:01 -0500

    Balance: Add MaterialUI

--- src/components/balance/balance.jsx
+++ src/components/balance/balance.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import '../../styles/balance.css';
 import Paper from 'material-ui/Paper';
+import { List, ListItem } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
 import CircularProgress from 'material-ui/CircularProgress';
-
 
 class Balance extends React.Component {
   render() {
@@ -15,11 +16,18 @@ class Balance extends React.Component {
     ) :
     info.address ? (
       <div className="balance">
-       <Paper className="paper" zDepth={1} >
-        <label>Address: {this.props.info.address}</label><br/>
-        <label>Total Received: {this.props.info.total_received / 100000000} BTC</label><br/>
-        <label>Total Sent: {this.props.info.total_sent / 100000000} BTC</label><br/>
-        <label>Remaining Balance: {this.props.info.final_balance / 100000000} BTC</label><br/>
+        <Paper className="paper" zDepth={1} >
+          <List>
+            <label>Balance</label>
+            <Divider />
+            <ListItem
+              nestedItems={[
+                <ListItem>Address: {this.props.info.address}</ListItem>,
+                <ListItem>Total Received: {this.props.info.total_received / 100000000} BTC</ListItem>,
+                <ListItem>Total Sent: {this.props.info.total_sent / 100000000} BTC</ListItem>,
+                <ListItem>Remaining Balance: {this.props.info.final_balance / 100000000} BTC</ListItem>,
+              ]}/>
+          </List>
         </Paper>
       </div>
     ) : null;
