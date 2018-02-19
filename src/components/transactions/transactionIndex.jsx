6ebd5fef8b5d290003c3829461dfaab68eda16ff
Terence Buchannan <terencebuchannan@gmail.com>
2018-02-18 23:12:09 -0500

    Transactions: Refactored to MaterialUI Table

--- src/components/transactions/transactionIndex.jsx
+++ src/components/transactions/transactionIndex.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import TransactionItem from './transactionItem.jsx';
 import List from 'material-ui/List';
+import '../../styles/transactions.css';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
 class TransactionIndex extends React.Component {
   render() {
     const { isLoading, transactions } = this.props;
+    const centerCol = {textAlign: 'center'};
     return isLoading ? null : (
-      <List className="listIndex">
-        <label>Transactions</label>
-        {transactions.map((item, idx) =>
-            <TransactionItem key={idx} info={item} random={idx} />
-          )}
-      </List>
+      <div className="listIndex">
+        <Table  fixedFooter={false} height={'300px'}>
+          <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+            <TableRow>
+              <TableHeaderColumn
+                style={centerCol}
+                colSpan="3"> Transactions
+              </TableHeaderColumn>
+            </TableRow>
+            <TableRow>
+              <TableHeaderColumn style={centerCol}>Transaction</TableHeaderColumn>
+              <TableHeaderColumn style={centerCol}>Received Time</TableHeaderColumn>
+              <TableHeaderColumn style={centerCol}>Received Date</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox={false}>
+            {transactions.map((item, idx) =>
+              <TransactionItem key={idx} info={item} rowKey={idx} />
+            )}
+          </TableBody>
+        </Table>
+      </div>
     );
   }
 }
--- src/components/transactions/transactionItem.jsx
+++ src/components/transactions/transactionItem.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import Paper from 'material-ui/Paper';
 import '../../styles/transactions.css';
 import ListItem from 'material-ui/List';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
 class TransactionItem extends React.Component {
   render() {
-    const { info } = this.props;
+    const { info, rowKey } = this.props;
     let date = new Date(info.time * 1000);
     return (
-      <ListItem>
-        <Paper className="transPaper" zDepth={1} >
-          <label>Transaction number: {info.hash} </label>
-
-        <label>Received Time: {date.toLocaleDateString()} {date.toLocaleTimeString()}</label>
-        </Paper>
-      </ListItem>
+      <TableRow key={rowKey}>
+        <TableRowColumn style={{textAlign: 'center'}}>{info.hash}</TableRowColumn>
+        <TableRowColumn style={{textAlign: 'center'}}>{date.toLocaleDateString()}</TableRowColumn>
+        <TableRowColumn style={{textAlign: 'center'}}>{date.toLocaleTimeString()}</TableRowColumn>
+      </TableRow>
     );
   }
 }
 export default TransactionItem;
+
+// <ListItem>
+//   <Paper className="transPaper" zDepth={1} >
+//     <label>Transaction number: {info.hash} </label>
+//
+//     <label>Received Time: {} {}</label>
+//   </Paper>
+// </ListItem>
