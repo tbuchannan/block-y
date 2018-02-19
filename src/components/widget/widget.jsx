585d1c0647bc92b91acecbbe6a59f8ba0c9de1ad
Terence Buchannan <terencebuchannan@gmail.com>
2018-02-19 01:28:18 -0500

    Widget: Add socket, switched MaterialUI loaders

--- src/components/widget/widget.jsx
+++ src/components/widget/widget.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import LiveTransactionIndex from '../livetransactions/liveTransactionIndex.jsx';
 import TransactionIndex from '../transactions/transactionIndex.jsx';
+import LinearProgress from 'material-ui/LinearProgress';
 import Balance from '../balance/balance.jsx';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -14,38 +14,53 @@ class Widget extends React.Component {
       address: "",
       balance: {},
       transactions: [],
-      liveTransactions: [],
-      subs: [],
       errors: null,
       isLoading: false
     };
     this.socket = new WebSocket('wss://ws.blockchain.info/inv');
-    this.handleSubmit = this.handleSubmit.bind(this);
     this.fetchBalance = this.fetchBalance.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleMessage = this.handleMessage.bind(this);
     this.handleErrors = this.handleErrors.bind(this);
+  }
+  componentDidMount() {
+    // Event Listeners for socket events (Open, Close, Receive Message)
+    this.socket.onopen = () => {
+      console.log("CONNECTED");
+    };
+
+    this.socket.onclose = () => {
+      console.log("DISCONNECTED");
+    };
+
+    this.socket.onmessage = (message) => {
+      let parsedMessage = JSON.parse(message.data);
+      this.handleMessage(parsedMessage);
+    };
+  }
+
+  fetchBalance() {
+    fetch(`/rawaddr/${this.state.address}`)
+      .then(this.handleErrors)
+      .then(result => result.json())
+      .then(data => this.setState({balance: data, transactions: data.txs, isLoading: false, address: "", errors: null}))
+      .catch(error => this.setState({balance: {}, errors: error.toString(), isLoading: false, address: ""}));
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    this.socket.send(`{"op":"addr_unsub", "addr":"${this.state.address}"}`);
     this.socket.send(`{"op":"addr_sub", "addr":"${this.state.address}"}`);
-    // this.socket.send(`{"op":"unconfirmed_sub"}`);
     this.setState({isLoading: true}, this.fetchBalance);
-  }
-
-  fetchBalance() {
-    const newSubs = [...this.state.subs, this.state.address];
-    fetch(`/rawaddr/${this.state.address}`)
-      .then(this.handleErrors)
-      .then(result => result.json())
-      .then(data => this.setState({balance: data, transactions: data.txs, subs: newSubs, isLoading: false, address: ""}))
-      .catch(error => this.setState({balance: {}, errors: error.toString(), isLoading: false, address: ""}));
   }
 
   handleChange(e) {
     e.preventDefault();
     this.setState({address: e.target.value});
+  }
+
+  handleMessage(newTx){
+    this.setState({transactions: [newTx].concat(this.state.transactions)});
   }
 
   handleErrors(resp) {
@@ -56,11 +71,14 @@ class Widget extends React.Component {
   }
 
   render() {
+    const style = !this.state.isLoading ? {display: 'none'} : null;
+    const barStyle = this.state.isLoading ? "hidden" : "greyBar";
     return (
       <div>
+        <LinearProgress style={style} mode="indeterminate" />
+        <div className={barStyle}></div>
         <div className ="searchContainer">
           <TextField
-            className="searchText"
             hintText="Enter bitcoin address"
             errorText={this.state.errors}
             onChange={this.handleChange}
@@ -69,17 +87,12 @@ class Widget extends React.Component {
         <br/>
           <RaisedButton label="Search" primary={true} onClick={this.handleSubmit}/>
         </div>
-
-        <Balance info={this.state.balance} isLoading={this.state.isLoading}/>
-        <div className ="transactionContainer">
+        <div className="infoContainer">
           <TransactionIndex
+            className="transIndex"
             transactions={this.state.transactions}
-            isLoading={this.state.isLoading}
-            />
-          <LiveTransactionIndex
-            liveTransactions={this.state.liveTransactions}
-            isLoading={this.state.isLoading}
-            socket={this.socket}/>
+            handleMessage={this.handleMessage}/>
+          <Balance info={this.state.balance}/>
         </div>
       </div>
     );
