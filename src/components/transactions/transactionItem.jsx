df87548b4839b75d4ec66ff461f2f940233ad89e
Terence Buchannan <terencebuchannan@gmail.com>
2018-02-20 14:15:38 -0500

    TransactionItem: Correct hardcoded time

--- src/components/transactions/transactionItem.jsx
+++ src/components/transactions/transactionItem.jsx
@@ -2,27 +2,30 @@ import React from 'react';
 import '../../styles/transactions.css';
 import { TableRow, TableRowColumn } from 'material-ui/Table';
 
+const _hashStyle = {
+  textAlign: 'center',
+  whiteSpace: 'normal',
+  width: '65%',
+  wordWrap: 'break-word'
+};
+
 class TransactionItem extends React.Component {
   render() {
-    const { data, rowKey} = this.props;
-    const hashStyle = {
-      textAlign: 'center',
-      whiteSpace: 'normal',
-      width: '65%',
-      wordWrap: 'break-word'};
-    let date = new Date(56564454556 * 1000);
+    const { data, rowKey } = this.props;
+
+    let date = new Date(data.time * 1000);
 
     // Parse transaction data and display as row
     return (
-      <TableRow key={rowKey}>
-        <TableRowColumn style={hashStyle}>{data.hash}</TableRowColumn>
+      <TableRow key={ rowKey }>
+        <TableRowColumn style={ _hashStyle }>{ data.hash }</TableRowColumn>
         <TableRowColumn
-          style={{textAlign: 'center', width: '19%',}}>
-            {date.toLocaleDateString()}
+          style={ { textAlign: 'center', width: '19%' } }>
+            { date.toLocaleDateString() }
         </TableRowColumn>
         <TableRowColumn
-          style={{textAlign: 'center', width: '20%',}}>
-          {date.toLocaleTimeString()}
+          style={ { textAlign: 'center', width: '20%' } }>
+          { date.toLocaleTimeString() }
         </TableRowColumn>
       </TableRow>
     );
